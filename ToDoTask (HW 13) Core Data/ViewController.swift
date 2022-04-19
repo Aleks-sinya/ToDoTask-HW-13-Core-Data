222c6e0cf7d33f971379b604b045338ee0a4ed62
Aleks.sinya <sinyagovskiy93@mail.ru>
2022-04-19 19:29:31 +0700

    Initial Commit

---
+++ ToDoTask (HW 13) Core Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoTask (HW 13) Core Data
+//
+//  Created by Алексей Синяговский on 19.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
